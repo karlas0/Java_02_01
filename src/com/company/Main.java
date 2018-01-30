224e77cd99fa9d0ccfed9121ed9569d0ad6a4fd8
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 14:35:00 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Sveiki, iveskite savo varda");
+
+        Scanner sc = new Scanner(System.in);
+
+        String vardas = sc.nextLine();
+
+        System.out.println(vardas);
+
+        System.out.println(vardas);
+
+        System.out.println(vardas);
+
+        System.out.println(vardas);
+
+        System.out.println(vardas);
     }
 }
