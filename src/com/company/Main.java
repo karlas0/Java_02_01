74693fe1e605c99ca6332c00a07f6f0d6352b0e7
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-31 11:49:46 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,11 +11,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String vardas = sc.nextLine();
-        for (int i = 0; i < 5; i++) {
 
-            System.out.println(vardas);
+        System.out.println(vardas);
 
+        System.out.println(vardas);
 
-        }
+        System.out.println(vardas);
+
+        System.out.println(vardas);
+
+        System.out.println(vardas);
+
     }
 }
